c39b5349b45c33f10de6bdb16ee54d8284be2796
Valdoveste <nickyv.oliveira@gmail.com>
2020-09-20 18:43:31 -0300

    Search images in DB now are available.

--- src/php/db_actions/db_consultation.php
+++ src/php/db_actions/db_consultation.php
@@ -1,7 +1,22 @@
 <?php
+    session_start();
+
     require_once './db_connection.php';
 
     if(isset($_POST["btn-consultation"])){
+        $imgCodigo = mysqli_escape_string($connection, $_POST['imgCodigo']);
+
+        $sql = "SELECT * FROM images WHERE idimg = '$imgCodigo'";
+        $result = mysqli_query($connection, $sql);
         
-    }
+        if(mysqli_num_rows($result) > 0){
+            $data = mysqli_fetch_array($result);
+            $_SESSION['imgData'] = $data;
+            header('Location: ../consultation.php?sucesso=1');
+        }else{
+            echo "Codigo";
+        }
+    }else{
+        echo "Button";
+    } 
 ?>
